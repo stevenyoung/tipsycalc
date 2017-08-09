c1d977aaec3d920f8e9bba8fbf1c52d82201c565
Steven Young <fivepockets@gmail.com>
2017-08-09 12:19:09 -0700

    Initial Commit

---
+++ tipsycalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipsycalc
+//
+//  Created by Steven Young on 8/9/17.
+//  Copyright © 2017 Steven Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
